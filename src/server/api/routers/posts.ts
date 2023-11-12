8d46c6add7e3c307a54513e11356612aca62033d
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-13 07:57:32 +0800

    added user profile image and message input

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,37 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/server";
+
+const filterUserForClient = (user: User) => {
+    return{
+        id: user.id,
+        username: user.username,
+        profileImageUrl: user.imageUrl,
+    };
+};
+
 
 export const postsRouter = createTRPCRouter({
     getAll: publicProcedure
-    .query(({ ctx }) => {
-        return ctx.db.post.findMany();
+    .query(async ({ ctx }) => {
+        const posts = await ctx.db.post.findMany({
+            take: 100,
+        });
+
+        const users = (
+            await clerkClient.users.getUserList({
+                userId: posts.map((post) => post.authorId),
+                limit: 100,
+            })
+        ).map(filterUserForClient)
+
+        console.log(users)
+
+        return posts.map((post) => ({
+            post, 
+            author: users.find((user) => user.id === post.authorId)!,
+        }))
     })
 });
+
