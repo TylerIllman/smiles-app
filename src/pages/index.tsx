e89677fc013d79f2ea1247e4b8508e9f7f8dc4ee
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-12 21:35:50 +0800

    Attempted fix of fetch all posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,6 @@ export default function Home() {
 
   const { data } = api.posts.getAll.useQuery()
 
-
   return (
     <>
       <Head>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const postsRouter = createTRPCRouter({
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.post.findmany();
-    }),
+    getAll: publicProcedure
+    .query(({ ctx }) => {
+        return ctx.db.post.findMany();
+    })
 });
