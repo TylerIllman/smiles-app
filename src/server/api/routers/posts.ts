73ab065349bc42642f8b3c2e3419fb39e729e5aa
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-13 08:50:40 +0800

    added usernames to posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,11 +2,13 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 
 const filterUserForClient = (user: User) => {
     return{
         id: user.id,
-        username: user.username,
+        // CURRENTLY PULLING FIRST NAME becuase no username in Google account. Need to add a setup wizard 
+        username: user.firstName,
         profileImageUrl: user.imageUrl,
     };
 };
@@ -28,10 +30,19 @@ export const postsRouter = createTRPCRouter({
 
         console.log(users)
 
-        return posts.map((post) => ({
-            post, 
-            author: users.find((user) => user.id === post.authorId)!,
-        }))
+        return posts.map((post) => {
+            const author = users.find((user) => user.id === post.authorId);
+
+            if (!author || !author.username) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Author for post not found"});
+
+            return {
+                post, 
+                author: {
+                    ...author,
+                    username: author.username,
+                }
+            };
+        })
     })
 });
 
