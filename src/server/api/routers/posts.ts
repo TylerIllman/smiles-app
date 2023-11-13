72bc110075b0b4d59bed1ba73d55740cfff8044e
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-13 16:20:11 +0800

    actually added the optional chaining

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -46,7 +46,7 @@ export const postsRouter = createTRPCRouter({
         return posts.map((post) => {
             const author = users.find((user) => user.id === post.authorId);
 
-            if (!author || !author?.username) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Author for post not found"});
+            if (!author?.username) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Author for post not found"});
 
             return {
                 post, 
