eeaa42a21c7090a77ca5163dabae4f806dd05e60
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-13 15:20:51 +0800

    changed error msg

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -61,14 +61,13 @@ export const postsRouter = createTRPCRouter({
     create: privateProcedure
         .input(
             z.object({
-                content: z.string().emoji().min(1).max(255),
+                content: z.string().emoji("Only emojis are allowed!").min(1).max(255),
             })
         )
         .mutation(async ({ ctx, input }) => {
         const authorId = ctx.currentUser;
 
         const { success } = await ratelimit.limit(authorId)
-
         if (!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"})
 
         const post = await ctx.db.post.create({
